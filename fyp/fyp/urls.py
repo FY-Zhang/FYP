70b574eeef794162bc82ca023a1a1b042477f1d9
FY_Zhang <50323953+FY-Zhang@users.noreply.github.com>
2021-02-16 09:01:28 +0800

    More Content

--- fyp/fyp/urls.py
+++ fyp/fyp/urls.py
@@ -3,7 +3,9 @@ from . import views
  
 urlpatterns = [
     path('', views.index, name='index'),
+    path('article', views.article, name='article'),
     path('login', views.login, name='login'),
+    path('register', views.register, name='register'),
     path('forum', views.forum, name='forum'),
     path('send_post', views.send_post, name='send_post'),
 ]
--- fyp/fyp/views.py
+++ fyp/fyp/views.py
@@ -3,8 +3,14 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
+def article(request):
+    return render(request, 'article.html')
+
 def login(request):
     return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
 
 def forum(request):
     return render(request, 'forum.html')
