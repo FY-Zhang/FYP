bfcb173143032450e0e4bd4124aa1d30e339bb61
FY_Zhang <50323953+FY-Zhang@users.noreply.github.com>
2021-03-14 09:09:58 +0800

    Fix Forum

--- fyp/fyp/urls.py
+++ fyp/fyp/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('forum', views.forum, name='forum'),
     path('post', views.post, name='post'),
     path('send_post', views.send_post, name='send_post'),
+    path('my_post', views.my_post, name='my_post'),
+    path('delete_post', views.delete_post, name='delete_post'),
 ]
--- fyp/fyp/views.py
+++ fyp/fyp/views.py
@@ -8,6 +8,11 @@ def photos(request):
     return render(request, 'photos.html')
 
 def login(request):
+    status = request.COOKIES.get('is_login')
+    if not status:
+        cookie = ''
+    else:
+        cookie = status
     error_msg=''
     if request.method == 'POST':
         email = request.POST.get('email')
@@ -21,7 +26,7 @@ def login(request):
         else:
             error_msg = 'Wrong email or password'
             return render(request, 'login.html', {'error_msg': error_msg})
-    return render(request, 'login.html')
+    return render(request, 'login.html', {'cookie': cookie})
 
 def logout(request):
     rep = redirect('/forum')
@@ -29,6 +34,11 @@ def logout(request):
     return rep
 
 def register(request):
+    status = request.COOKIES.get('is_login')
+    if not status:
+        cookie = ''
+    else:
+        cookie = status
     error_msg = ''
     if request.method == 'POST':
         user = request.POST.get('username')
@@ -46,7 +56,7 @@ def register(request):
             user = models.User.objects.create(user_email=email, user_name=user, user_password=password)
             user.save()
             return redirect('/login')
-    return render(request, 'register.html')
+    return render(request, 'register.html', {'cookie': cookie})
 
 def forum(request):
     posts = models.Post.objects.all().order_by('-id')   # 倒序取数据
@@ -69,20 +79,28 @@ def post(request):
     for user in users:
         if user.user_email == post.user_email:
             break
-    # 存入评论
     if request.method == 'POST':
-        comment = request.POST.get('comment')
-        comments = models.Post_Comment.objects.create(post_id=id, user_email=status, comment_content = comment)
-        comments.save()
-        return redirect('/post?id='+str(id))
+        # 存入评论
+        if request.POST.get('commentBtn') == 'comment':
+            comment = request.POST.get('comment')
+            comments = models.Post_Comment.objects.create(post_id=id, user_email=status, comment_content = comment)
+            comments.save()
+            return redirect('/post?id='+str(id))
+        # 修改帖子
+        elif request.POST.get('editBtn') == 'edit':
+            title = request.POST.get('title')
+            topic = request.POST.get('topic')
+            content = request.POST.get('content')
+            models.Post.objects.filter(id=id).update(post_title=title, post_topic=topic, post_content=content, user_email=status)
+            return redirect('/post?id='+str(id))
     # 读取评论
-    post_comments = models.Post_Comment.objects.all().order_by('-id')   # 倒序取数据
+    post_comments = models.Post_Comment.objects.filter(post_id=id).order_by('-id')   # 倒序取数据
     # 获取Cookie
     if not status:
         cookie = ''
     else:
         cookie = status
-    return render(request, 'post.html', {'post': post, 'user': user, 'cookie': cookie, 'users': users, 'post_comments': post_comments})
+    return render(request, 'post.html', {'post': post, 'user': user, 'status': status, 'cookie': cookie, 'users': users, 'post_comments': post_comments})
 
 def send_post(request):
     status = request.COOKIES.get('is_login')
@@ -99,3 +117,17 @@ def send_post(request):
     else:
         cookie = status
     return render(request, 'send_post.html', {'cookie': cookie})
+
+def my_post(request):
+    status = request.COOKIES.get('is_login')
+    posts = models.Post.objects.filter(user_email=status).order_by('-id')   # 倒序取数据
+    if not status:
+        cookie = ''
+    else:
+        cookie = status
+    return render(request, 'my_post.html', {'cookie': cookie, 'posts': posts})
+
+def delete_post(request):
+    id = int(request.GET.get('id'))
+    models.Post.objects.filter(id=id).delete()
+    return redirect('/forum')
