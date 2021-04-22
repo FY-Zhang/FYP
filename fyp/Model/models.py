656b936b8009167537da3c7d91185cbfd86ff90c
FY_Zhang <50323953+FY-Zhang@users.noreply.github.com>
2021-04-22 17:46:53 +0800

    change database

--- fyp/Model/models.py
+++ fyp/Model/models.py
@@ -5,13 +5,8 @@ from django.db import models
 class User(models.Model):
     user_email = models.CharField(max_length=50)
     user_name = models.CharField(max_length=50)
-    user_password = models.CharField(max_length=200)
+    user_password = models.CharField(max_length=50)
     user_type = models.IntegerField(default=0)  #默认普通用户，1是管理员
-
-class User_Collection(models.Model):
-    user_email = models.CharField(max_length=50)
-    post_id = models.IntegerField(default=0)
-    collection_time = models.DateTimeField(auto_now_add=True)   #收藏时间
 
 class Post(models.Model):
     post_title = models.CharField(max_length=200)
@@ -19,9 +14,6 @@ class Post(models.Model):
     post_content = models.TextField()
     post_time = models.DateTimeField(auto_now_add=True) #发帖时间
     user_email = models.CharField(max_length=50)
-    like_num = models.IntegerField(default=0)   #获赞数量
-    comment_num = models.IntegerField(default=0)    #评论数量
-    collection_num = models.IntegerField(default=0) #收藏数量
 
 class Post_Comment(models.Model):
     post_id = models.IntegerField(default=0)
